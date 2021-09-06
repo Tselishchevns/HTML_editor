cc68fccbc70650ee1d2d39f9bea3df0c37fa2105
Nikita <tselishchevns@gmail.com>
2021-09-06 21:46:55 +0300

    Initial commit

---
+++ src/com/company/listeners/TextEditMenuListener.java
@@ -0,0 +1,26 @@
+package com.company.listeners;
+
+import com.company.View;
+
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+public class TextEditMenuListener implements MenuListener {
+
+    public TextEditMenuListener(View view){}
+
+    @Override
+    public void menuSelected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+
+    }
+}
